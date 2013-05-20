8b0df7ae8a1be38960bd7a6983aa3cf1d546af83
Jake Jarvis <jakejarvis@gmail.com>
2013-05-19 22:39:46 -0400

    responsive footer

--- footer.php
+++ footer.php
@@ -4,23 +4,26 @@
 
       <div class="footer">
         <div class="row hidden-phone">
-            <div class="span4 align-left">
-                <p>Baked with <span style="color:#79ad36">&hearts;</span> by <a href="http://scrabble.io/" target="_blank">scribscrab</a>.<span class="visible-desktop" style="display:inline !important;">&nbsp;&nbsp;&nbsp;<a href="https://github.com/jakejarvis/spoons/issues" target="_blank">Report an issue</a>.</span></p>
+            <div class="span4 align-left visible-desktop">
+              <p>Baked with <span style="color:#79ad36">&hearts;</span> by <a href="http://scrabble.io/" target="_blank">scribscrab</a>.&nbsp;&nbsp;&nbsp;<a href="https://github.com/jakejarvis/spoons/issues" target="_blank">Report an issue</a>.</p>
+            </div>
+            <div class="span4 align-left visible-tablet">
+              <p>Baked with <span style="color:#79ad36">&hearts;</span> by <a href="http://scrabble.io/" target="_blank">scribscrab</a>.</p>
             </div>
             <div class="span4 align-center">
-                <a href="http://www.internaldrive.com/" target="_blank"><img src="<?php echo $site_url ?>/assets/img/id.png" alt="internalDrive"></a>
+              <a href="http://www.internaldrive.com/" target="_blank"><img src="<?php echo $site_url ?>/assets/img/id.png" alt="internalDrive"></a>
             </div>
             <div class="span4 align-right">
-                <p><a href="#">Back to top!</a></p>
+              <p><a href="#">Back to top!</a></p>
             </div>
         </div>
         
         <div class="row visible-phone">
             <div class="span12 align-center">
-                <p>Baked with <span style="color:#79ad36">&hearts;</span> by <a href="http://scrabble.io/" target="_blank">scribscrab</a>.</p>
+              <p>Baked with <span style="color:#79ad36">&hearts;</span> by <a href="http://scrabble.io/" target="_blank">scribscrab</a>.</p>
             </div>
             <div class="span12 align-center">
-                <p><a href="http://www.internaldrive.com/" target="_blank"><img src="<?php echo $site_url ?>/assets/img/id.png" alt="internalDrive"></a></p>
+              <p><a href="http://www.internaldrive.com/" target="_blank"><img src="<?php echo $site_url ?>/assets/img/id.png" alt="internalDrive"></a></p>
             </div>
         </div>
       </div>
