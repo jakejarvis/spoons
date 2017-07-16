5e8c9fc105d7a4110e8fb25b08fac832e9582ece
Jake Jarvis <jakejarvis@gmail.com>
2017-07-16 14:31:50 -0400

    set database timezone to east coast beast coast

--- public/db_connect.php
+++ public/db_connect.php
@@ -1,17 +1,11 @@
 <?php
-//mysql_connect($db_host . ":" . $db_port, $db_user, $db_pass);
-//mysql_select_db($db_name);
+  // Create connection
+  $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-//mysql_query('SET time_zone = "' . $timezone_number . '"');
-
-
-// Create connection
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-// Check connection
-if (!$conn) {
+  // Check connection
+  if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-}
+  }
 
-
+  mysqli_query($conn, 'SET time_zone = "' . $timezone_number . '"');
 ?>
