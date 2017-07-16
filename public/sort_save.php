3eece2564ac4db5dc245af1b02f438b71dc18326
Jake Jarvis <jakejarvis@gmail.com>
2017-07-16 19:47:55 -0400

    sorting fix

--- public/sort_save.php
+++ public/sort_save.php
@@ -5,6 +5,6 @@ include('db_connect.php');
 $order_array = explode(",", $_POST['order']);
 foreach($order_array as $key => $value) {
   echo $value . ' > ' . $key . "\n";
-  mysqli_query($conn, ("UPDATE spooners SET order_num = " . $key . " WHERE id = " . $value) or die(mysqli_error($conn));
+  mysqli_query($conn, "UPDATE spooners SET order_num = " . $key . " WHERE id = " . $value) or die(mysqli_error($conn));
 }
 ?>
